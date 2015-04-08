7c4225ea675aa4723ca8606d949aa292a816a22b
Christopher M. Judd <javajudd@gmail.com>
2015-04-07 22:42:47 -0400

    Added some simple authentication.

--- src/main/java/portero/web/MvcConfig.java
+++ src/main/java/portero/web/MvcConfig.java
@@ -11,5 +11,6 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
     }
 }
