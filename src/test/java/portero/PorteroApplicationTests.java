06e596a0f20365dc96e25884cf3b5d3e895d9fc8
Christopher M. Judd <javajudd@gmail.com>
2015-04-07 21:35:49 -0400

    Initial project generated by start.spring.io.

---
+++ src/test/java/portero/PorteroApplicationTests.java
@@ -0,0 +1,18 @@
+package portero;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PorteroApplication.class)
+@WebAppConfiguration
+public class PorteroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
