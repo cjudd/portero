b149078c5a2a2fd03238fc4ff7a9be7e61e98267
Christopher M. Judd <javajudd@gmail.com>
2015-04-09 19:52:00 -0400

    Added initial detail page.

--- src/main/java/portero/dao/OpportunityRepository.java
+++ src/main/java/portero/dao/OpportunityRepository.java
@@ -2,10 +2,12 @@ package portero.dao;
 
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import portero.domain.Opportunity;
 
 import java.util.List;
 
+@Repository
 public interface OpportunityRepository extends CrudRepository<Opportunity, String> {
 
     List<Opportunity> findByActive(boolean status);
--- src/main/java/portero/web/controllers/IndexController.java
+++ src/main/java/portero/web/controllers/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import portero.dao.OpportunityRepository;
 import portero.domain.Opportunity;
 
@@ -30,6 +31,14 @@ public class IndexController {
         model.addAttribute("opportunities", opportunities);
 
         return "home";
+    }
+
+    @RequestMapping("detail")
+    public String detail(@RequestParam(value = "id", required = true) String id, Model model) {
+        Opportunity opportunity = repository.findOne(id);
+        model.addAttribute("opportunity", opportunity);
+
+        return "detail";
     }
 
 }
