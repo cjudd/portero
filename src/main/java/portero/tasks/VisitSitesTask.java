97766d48834499a52785722f61ab9802aaaf8f6e
Christopher M. Judd <javajudd@gmail.com>
2015-04-27 17:15:51 -0400

    Added a scheduled task that keeps a hijacked http session alive.

---
+++ src/main/java/portero/tasks/VisitSitesTask.java
@@ -0,0 +1,49 @@
+package portero.tasks;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import portero.dao.OpportunityRepository;
+import portero.domain.Opportunity;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static java.lang.String.format;
+
+@Component
+public class VisitSitesTask {
+
+    public static final Log log = LogFactory.getLog(VisitSitesTask.class);
+
+    public static final int FIVE_MINUTES = 5000;// 600000;
+
+    @Autowired
+    private OpportunityRepository repository;
+
+    @Scheduled(fixedRate = FIVE_MINUTES)
+    public void reportCurrentTime() throws IOException {
+
+        for (Opportunity opportunity: repository.findByActive(true)) {
+            log.info(format("visiting %s - %s - %s", opportunity.getId(), opportunity.getUrl(), opportunity.getCookies()));
+
+            URL url = new URL(opportunity.getUrl());
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestProperty("Cookie", opportunity.getCookies());
+            connection.connect();
+
+            int responseCode = connection.getResponseCode();
+            if( responseCode < 200 || responseCode > 400){
+                log.error(format("failed %s - %s with %d", opportunity.getId(), opportunity.getUrl(), responseCode));
+                opportunity.setActive(false);
+                repository.save(opportunity);
+            }
+
+        }
+
+    }
+
+}
--- src/main/java/portero/web/MvcConfig.java
+++ src/main/java/portero/web/MvcConfig.java
@@ -1,10 +1,12 @@
 package portero.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableScheduling
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
