7d752d4ec05ab0e52819206355f17b9aa04e7719
Christopher M. Judd <javajudd@gmail.com>
2015-04-28 15:50:22 -0400

    Added configurations to create a war file for some type of testing.

--- src/main/java/portero/PorteroApplication.java
+++ src/main/java/portero/PorteroApplication.java
@@ -2,11 +2,18 @@ package portero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PorteroApplication {
+public class PorteroApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PorteroApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PorteroApplication.class);
     }
 }
